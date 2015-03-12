dddbf0357fc6380098ef4090862d54ee7d13611e
Ricardo J. Mendez <ricardo@numergent.com>
2015-03-13 01:07:32 +0200

    Reformatted project file, long overdue

--- project.clj
+++ project.clj
@@ -1,82 +1,82 @@
 (defproject tropefest "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+            :description "FIXME: write description"
+            :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring-server "0.3.1"]
-                 [selmer "0.8.0"]
-                 [com.taoensso/timbre "3.3.1"]
-                 [com.taoensso/tower "3.0.2"]
-                 [markdown-clj "0.9.64"]
-                 [environ "1.0.0"]
-                 [im.chit/cronj "1.4.3"]
-                 [compojure "1.3.2"]
-                 [ring/ring-defaults "0.1.3"]
-                 [ring/ring-session-timeout "0.1.0"]
-                 [ring-middleware-format "0.4.0"]
-                 [noir-exception "0.2.3"]
-                 [bouncer "0.3.2"]
-                 [prone "0.8.0"]
-                 [enlive "1.1.5"]
-                 [clojurewerkz/neocons "3.1.0-beta2"]
-                 [clojurewerkz/urly "2.0.0-alpha5"]
-                 [clojure.joda-time "0.2.0"]
-                 ]
+            :dependencies [[org.clojure/clojure "1.6.0"]
+                           [ring-server "0.3.1"]
+                           [selmer "0.8.0"]
+                           [com.taoensso/timbre "3.3.1"]
+                           [com.taoensso/tower "3.0.2"]
+                           [markdown-clj "0.9.64"]
+                           [environ "1.0.0"]
+                           [im.chit/cronj "1.4.3"]
+                           [compojure "1.3.2"]
+                           [ring/ring-defaults "0.1.3"]
+                           [ring/ring-session-timeout "0.1.0"]
+                           [ring-middleware-format "0.4.0"]
+                           [noir-exception "0.2.3"]
+                           [bouncer "0.3.2"]
+                           [prone "0.8.0"]
+                           [enlive "1.1.5"]
+                           [clojurewerkz/neocons "3.1.0-beta2"]
+                           [clojurewerkz/urly "2.0.0-alpha5"]
+                           [clojure.joda-time "0.2.0"]
+                           ]
 
-  :cljsbuild {:builds
-              [{:source-paths ["src-cljs"],
-                :id           "dev",
-                :compiler
-                              {:output-dir    "resources/public/js/",
-                               :optimizations :none,
-                               :output-to     "resources/public/js/app.js",
-                               :source-map    true,
-                               :pretty-print  true}}
-               {:source-paths ["src-cljs"],
-                :id           "release",
-                :compiler
-                              {:closure-warnings {:non-standard-jsdoc :off},
-                               :optimizations    :advanced,
-                               :output-to        "resources/public/js/app.js",
-                               :output-wrapper   false,
-                               :pretty-print     false}}]}
+            :cljsbuild {:builds
+                        [{:source-paths ["src-cljs"],
+                          :id           "dev",
+                          :compiler
+                                        {:output-dir    "resources/public/js/",
+                                         :optimizations :none,
+                                         :output-to     "resources/public/js/app.js",
+                                         :source-map    true,
+                                         :pretty-print  true}}
+                         {:source-paths ["src-cljs"],
+                          :id           "release",
+                          :compiler
+                                        {:closure-warnings {:non-standard-jsdoc :off},
+                                         :optimizations    :advanced,
+                                         :output-to        "resources/public/js/app.js",
+                                         :output-wrapper   false,
+                                         :pretty-print     false}}]}
 
-  :min-lein-version "2.0.0"
-  :uberjar-name "tropefest.jar"
-  :repl-options {:init-ns tropefest.handler}
-  :jvm-opts ["-server"]
+            :min-lein-version "2.0.0"
+            :uberjar-name "tropefest.jar"
+            :repl-options {:init-ns tropefest.handler}
+            :jvm-opts ["-server"]
 
-  :main tropefest.core
+            :main tropefest.core
 
-  :plugins [[lein-ring "0.9.1"]
-            [lein-cljsbuild "1.0.5"]
-            [lein-environ "1.0.0"]
-            [lein-ancient "0.6.0"]]
-
-
-  :ring {:handler tropefest.handler/app
-         :init    tropefest.handler/init
-         :destroy tropefest.handler/destroy
-         :uberwar-name "tropefest.war"}
+            :plugins [[lein-ring "0.9.1"]
+                      [lein-cljsbuild "1.0.5"]
+                      [lein-environ "1.0.0"]
+                      [lein-ancient "0.6.0"]]
 
 
+            :ring {:handler      tropefest.handler/app
+                   :init         tropefest.handler/init
+                   :destroy      tropefest.handler/destroy
+                   :uberwar-name "tropefest.war"}
 
-  :profiles
-  {:uberjar {:omit-source true
-             :env {:production true
-                   :db-url "https://54fe2764bbadc:M6vQ5ilIMd0RsQp45wz1CpfudRN2wMfo0Zn2QL59@neo-54fe2764bbadc-364459c455.do-stories.graphstory.com:7473/db/data/"
-                   }
 
-             :aot :all}
-   :production {:ring {:open-browser? false
-                       :stacktraces?  false
-                       :auto-reload?  false}}
-   :dev {:dependencies [[ring-mock "0.1.5"]
-                        [ring/ring-devel "1.3.2"]
-                        [pjstadig/humane-test-output "0.6.0"]
-                        ]
-         :injections [(require 'pjstadig.humane-test-output)
-                      (pjstadig.humane-test-output/activate!)]
-         :env {:dev true
-               :db-url "http://localhost:7474/db/data/"
-               }}})
+
+            :profiles
+            {:uberjar    {:omit-source true
+                          :env         {:production true
+                                        :db-url     "https://54fe2764bbadc:M6vQ5ilIMd0RsQp45wz1CpfudRN2wMfo0Zn2QL59@neo-54fe2764bbadc-364459c455.do-stories.graphstory.com:7473/db/data/"
+                                        }
+
+                          :aot         :all}
+             :production {:ring {:open-browser? false
+                                 :stacktraces?  false
+                                 :auto-reload?  false}}
+             :dev        {:dependencies [[ring-mock "0.1.5"]
+                                         [ring/ring-devel "1.3.2"]
+                                         [pjstadig/humane-test-output "0.6.0"]
+                                         ]
+                          :injections   [(require 'pjstadig.humane-test-output)
+                                         (pjstadig.humane-test-output/activate!)]
+                          :env          {:dev    true
+                                         :db-url "http://localhost:7474/db/data/"
+                                         }}})
