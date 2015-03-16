6901e487f9cf53b3a744c9f54f1c403f516446ad
Ricardo J. Mendez <ricardo@numergent.com>
2015-03-16 15:05:04 +0200

    Using lein-ancient to upgrade references

--- project.clj
+++ project.clj
@@ -3,24 +3,24 @@
             :url "http://example.com/FIXME"
 
             :dependencies [[org.clojure/clojure "1.6.0"]
-                           [ring-server "0.3.1"]
-                           [selmer "0.8.0"]
-                           [com.taoensso/timbre "3.3.1"]
+                           [ring-server "0.4.0"]
+                           [selmer "0.8.2"]
+                           [com.taoensso/timbre "3.4.0"]
                            [com.taoensso/tower "3.0.2"]
                            [markdown-clj "0.9.64"]
                            [environ "1.0.0"]
                            [im.chit/cronj "1.4.3"]
                            [compojure "1.3.2"]
-                           [ring/ring-defaults "0.1.3"]
+                           [ring/ring-defaults "0.1.4"]
                            [ring/ring-session-timeout "0.1.0"]
                            [ring-middleware-format "0.4.0"]
                            [noir-exception "0.2.3"]
                            [bouncer "0.3.2"]
-                           [prone "0.8.0"]
+                           [prone "0.8.1"]
                            [enlive "1.1.5"]
                            [clojurewerkz/neocons "3.1.0-beta2"]
                            [clojurewerkz/urly "2.0.0-alpha5"]
-                           [clojure.joda-time "0.2.0"]
+                           [clojure.joda-time "0.4.0"]
                            ]
 
             :cljsbuild {:builds
@@ -80,7 +80,7 @@
                           }
              :dev        {:dependencies [[ring-mock "0.1.5"]
                                          [ring/ring-devel "1.3.2"]
-                                         [pjstadig/humane-test-output "0.6.0"]
+                                         [pjstadig/humane-test-output "0.7.0"]
                                          ]
                           :injections   [(require 'pjstadig.humane-test-output)
                                          (pjstadig.humane-test-output/activate!)]
