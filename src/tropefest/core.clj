01357cee3717135b0fff37b9ae895140202c363a
Ricardo J. Mendez <ricardo@numergent.com>
2015-03-16 14:51:05 +0200

    Calling init when main is invoked directly
    Otherwise the application isn't properly initialized when running on
    uberjar.

--- src/tropefest/core.clj
+++ src/tropefest/core.clj
@@ -1,8 +1,9 @@
 (ns tropefest.core
-  (:require [tropefest.handler :refer [app]]
+  (:require [tropefest.handler :refer [app init]]
     [ring.adapter.jetty :refer [run-jetty]])
   (:gen-class))
 
 (defn -main [& args]
   (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
+    (init)                                                  ; Init doesn't get called on uberjar
     (run-jetty app {:port port :join? false})))
