b922167ab53080e1b7dc4e8df059b26a083ee6ea
Ricardo J. Mendez <ricardo@numergent.com>
2015-03-19 10:01:42 +0200

    New references for ClojureScript

--- project.clj
+++ project.clj
@@ -1,13 +1,15 @@
-(defproject tropefest "0.1.0-SNAPSHOT"
+(defproject tropefest "0.0.1-SNAPSHOT"
             :description "FIXME: write description"
             :url "http://example.com/FIXME"
 
             :dependencies [[org.clojure/clojure "1.6.0"]
+                           [org.clojure/clojurescript "0.0-3126"]
+                           [cljs-ajax "0.3.10"]
                            [ring-server "0.4.0"]
                            [selmer "0.8.2"]
                            [com.taoensso/timbre "3.4.0"]
                            [com.taoensso/tower "3.0.2"]
-                           [markdown-clj "0.9.64"]
+                           [markdown-clj "0.9.65"]
                            [environ "1.0.0"]
                            [im.chit/cronj "1.4.3"]
                            [compojure "1.3.2"]
@@ -21,7 +23,7 @@
                            [clojurewerkz/neocons "3.1.0-beta2"]
                            [clojurewerkz/urly "2.0.0-alpha5"]
                            [clojure.joda-time "0.4.0"]
-                           [http-kit "2.1.16"]
+                           [http-kit "2.1.19"]
                            ]
 
             :cljsbuild {:builds
@@ -70,6 +72,12 @@
                                         :update-cron "0 /3 * * * * *"
                                         :update-size 3
                                         }
+                          :hooks       ['leiningen.cljsbuild]
+                          :cljsbuild   {:jar true
+                                        :builds
+                                             {:app
+                                              {:source-paths ["env/prod/cljs"]
+                                               :compiler     {:optimizations :advanced :pretty-print false}}}}
                           :aot         :all}
              :production {:ring {:open-browser? false
                                  :stacktraces?  false
@@ -82,7 +90,16 @@
              :dev        {:dependencies [[ring-mock "0.1.5"]
                                          [ring/ring-devel "1.3.2"]
                                          [pjstadig/humane-test-output "0.7.0"]
+                                         [figwheel "0.2.5"]
                                          ]
+                          :plugins      [[lein-figwheel "0.2.3-SNAPSHOT"]]
+
+                          :cljsbuild    {:builds {:app {:source-paths ["env/dev/cljs"]}}}
+                          :figwheel
+                                        {:http-server-root "public"
+                                         :server-port      3449
+                                         :css-dirs         ["resources/public/css"]
+                                         :ring-handler     todelete.handler/app}
                           :injections   [(require 'pjstadig.humane-test-output)
                                          (pjstadig.humane-test-output/activate!)]
                           :env          {:dev         true
