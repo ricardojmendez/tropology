7be27c82c6a872f521f8508a9d8ba820c1937d9c
Ricardo J. Mendez <ricardo@numergent.com>
2015-03-20 18:49:46 +0200

    Added sigma.js, tested basic integration on home page

--- src-cljs/tropefest/core.cljs
+++ src-cljs/tropefest/core.cljs
@@ -24,7 +24,7 @@
   [:div
    "The atom " [:code "click-count"] " has value: "
    @click-count ". "
-   [:input {:type "button" :value "Click me!"
+   [:input {:type     "button" :value "Click me!"
             :on-click #(swap! click-count inc)}]])
 
 (defn timer-component []
@@ -32,7 +32,8 @@
     (fn []
       (js/setTimeout #(swap! seconds-elapsed inc) 1000)
       [:div
-       "Seconds Elapsed: " @seconds-elapsed]))) ()
+       "Seconds Elapsed: " @seconds-elapsed])))
+()
 
 (defn set-html! [el content]
   (set! (.-innerHTML el) content))
@@ -41,6 +42,10 @@
 
 ; Original code:
 
+(defn nav-item [tag label url]
+  [:li {:class (when (= tag (session/get :page)) "active")}
+   [:a {:on-click #(secretary/dispatch! (str "#/" url))} label]])
+
 (defn navbar []
   [:div.navbar.navbar-inverse.navbar-fixed-top
    [:div.container
@@ -48,10 +53,10 @@
      [:a.navbar-brand {:href "#/"} "tropefest"]]
     [:div.navbar-collapse.collapse
      [:ul.nav.navbar-nav
-      [:li {:class (when (= :home (session/get :page)) "active")}
-       [:a {:on-click #(secretary/dispatch! "#/")} "Home"]]
-      [:li {:class (when (= :about (session/get :page)) "active")}
-       [:a {:on-click #(secretary/dispatch! "#/about")} "About"]]]]]])
+      (nav-item :home "Home" "")
+      (nav-item :about "About" "about")
+      (nav-item :plot "Plot" "plot")
+      ]]]])
 
 (defn about-page []
   [:div
@@ -64,21 +69,26 @@
   [:div
    [:h2 "Welcome to ClojureScript"]])
 
+(defn plot []
+  [:div {:id "content"} "Empty"])
+
 (def pages
-  {:home home-page
-   :about about-page})
+  {:home  home-page
+   :about about-page
+   :plot  plot})
+
+
 
 (defn page []
   [(pages (session/get :page))])
 
 (defroute "/" [] (session/put! :page :home))
 (defroute "/about" [] (session/put! :page :about))
+(defroute "/plot" [] (session/put! :page :plot))
+(defroute "/plot" [] (session/put! :page :plot))
 
-
-(.log js/console "BEFORE INIT!")
 
 (defn init! []
-  (.log js/console "INITING!")
   (secretary/set-config! :prefix "#")
   (session/put! :page :home)
   (reagent/render-component [navbar] (.getElementById js/document "navbar"))
