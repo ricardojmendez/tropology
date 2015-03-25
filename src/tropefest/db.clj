5ec9eaf89364a717db50e553933b0e2307f0b807
Ricardo J. Mendez <ricardo@numergent.com>
2015-03-26 00:14:10 +0200

    Functions for querying nodes linked to and from a node

--- src/tropefest/db.clj
+++ src/tropefest/db.clj
@@ -48,15 +48,28 @@
     (merge {:timestamp now :nextupdate now} data)))
 
 ;
-; Query and creation functions
+; Node query and creation functions
 ;
+
+(defn id-to-match
+  "Returns match pattern string for an id, including the node label, with
+  the prefix name for the pattern.
+
+  I'm not a fan of not being able to pass the label as a parameter, but them's
+  the breaks. neo4j's parsing seems strong enough that code injection is
+  unlikely."
+  ([prefix id]
+   (id-to-match prefix id "id")
+    )
+  ([prefix id id-param]
+   (let [label (b/label-from-id id)]
+     (str "(" prefix ":" label " {id:{" id-param "}})"))))
 
 (defn query-by-id
   "Queries for a node id on the properties. Does not filter by label. Notice
   that this is not the same as getting the node directly via its internal id."
   [conn id]
-  (let [label (b/label-from-id id)
-        query-str (str "MATCH (v:" label " {id:{id}}) RETURN v") ; Not a fan of not being able to pass the label as a parameter, but them's the breaks
+  (let [query-str (str "MATCH  " (id-to-match "v" id) " RETURN v")
         match (first (cy/tquery conn query-str {:id id}))]
     (if (nil? match)
       nil
@@ -74,6 +87,25 @@
      (->> (cy/tquery conn "MATCH (v) WHERE v.isredirect = FALSE AND v.nextupdate < {now} RETURN v.url ORDER BY v.nextupdate LIMIT {limit}" {:now time-limit :limit node-limit})
           (map #(% "v.url")))
      '())))
+
+
+(defn query-from
+  "Retrieves the list of nodes that a node links to (emanating from)
+
+  We could probably write it getting the relationships and walking through
+  them, but going with cypher for now to test."
+  [conn ^String code rel]
+  (let [query-str (str "MATCH " (id-to-match "o" code "id") "-[" rel "]->(v) RETURN DISTINCT v.id as id,v.url as url, v.title as title")]
+    (cy/tquery conn query-str {:id code})))
+
+(defn query-to
+  "Retrieves the list of nodes that links to a node.
+  Yes, the parameter order is the opposite from query-links-from,
+  since I think it better indicates the relationship."
+  [conn rel ^String code]
+  (let [query-str (str "MATCH " (id-to-match "o" code "id") "<-[" rel "]-(v) RETURN DISTINCT v.id as id,v.url as url, v.title as title")]
+    (cy/tquery conn query-str {:id code})))
+
 
 (defn mark-if-redirect!
   "Marks all nodes identified by a URL as being a redirect, if true"
@@ -125,10 +157,18 @@
       (create-node! conn (:label data-items) data-items)
       (nn/get conn id))))
 
+
+
+;
+; Relationship functions
+;
+
 (defn relate-nodes!
   "Links two nodes by a relationship if they aren't yet linked"
   [conn relationship n1 n2]
   (nrl/maybe-create conn n1 n2 relationship))
+
+
 
 ; TODO: Add transaction support
 
