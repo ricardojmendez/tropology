f0deec833e5ec70cff45a4e08eb891d6dfe45903
Ricardo J. Mendez <ricardo@numergent.com>
2015-04-07 17:33:42 +0300

    New test environment settings
    - Allowing the container to use up to 4 cores for testing
    - Gave the test container a more specific name, as users could already
      have their own neo4j container

--- create-test-environment.sh
+++ create-test-environment.sh
@@ -1,3 +1,3 @@
 boot2docker start
 $(boot2docker shellinit)
-docker run -i -t -d --name neo4j --cap-add=SYS_RESOURCE -p 7373:7474 tpires/neo4j
+docker run -c 4 -i -t -d --name tropology-test --cap-add=SYS_RESOURCE -p 7373:7474 tpires/neo4j
