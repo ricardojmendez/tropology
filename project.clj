8574b39ac1b76d06e13dc2246e8eed92e849f474
Ricardo J. Mendez <ricardo@numergent.com>
2015-12-22 11:46:33 +0200

    Upgrading all non-Timbre dependencies

--- project.clj
+++ project.clj
@@ -3,36 +3,36 @@
   :url "http://numergent.com/tags/tropology/"
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.122"]
-                 [cljs-ajax "0.5.1"]
+                 [org.clojure/clojurescript "1.7.170"]
+                 [cljs-ajax "0.5.2"]
                  [ring-server "0.4.0"]
                  [com.taoensso/timbre "3.4.0"]
-                 [selmer "0.9.4"]
+                 [selmer "0.9.6"]
                  [com.taoensso/tower "3.0.2"]
-                 [markdown-clj "0.9.78"]
+                 [markdown-clj "0.9.82"]
                  [environ "1.0.1"]
-                 [im.chit/cronj "1.4.3"]
+                 [im.chit/cronj "1.4.4"]
                  [compojure "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
                  [ring/ring-session-timeout "0.1.0"]
                  [ring-middleware-format "0.7.0"]
                  [noir-exception "0.2.5"]
-                 [bouncer "0.3.3"]
-                 [prone "0.8.2"]
+                 [bouncer "1.0.0"]
+                 [prone "0.8.3"]
                  [enlive "1.1.6"]
                  [com.curiosity/urly "2.0.0-alpha6"]
                  [clojure.joda-time "0.6.0"]
                  [http-kit "2.1.19"]
                  [reagent "0.5.1" :exclusions [cljsjs/react]]
-                 [cljsjs/react-with-addons "0.14.0-1"]
+                 [cljsjs/react-with-addons "0.14.3-0"]
                  [reagent-utils "0.1.5"]
-                 [liberator "0.13"]
+                 [liberator "0.14.0"]
                  [cheshire "5.5.0"]
                  [korma "0.4.2"]
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]
                  [io.clojure/liberator-transit "0.3.0"]
-                 [re-frame "0.5.0"]
-                 [lein-doo "0.1.6-SNAPSHOT"]
+                 [re-frame "0.6.0"]
+                 [lein-doo "0.1.6"]
                  ]
 
   :source-paths ["src/clj" "src/cljs" "src/cljc"]
@@ -69,7 +69,7 @@
   :doo {:build "test"}
 
   :plugins [[lein-ring "0.9.7"]
-            [lein-cljsbuild "1.1.0"]
+            [lein-cljsbuild "1.1.2"]
             [lein-environ "1.0.1"]
             [lein-ancient "0.6.8"]
             [clj-sql-up "0.3.7"]
@@ -118,9 +118,9 @@
                 }
    :dev        {:dependencies [[ring-mock "0.1.5"]
                                [ring/ring-devel "1.4.0"]
-                               [pjstadig/humane-test-output "0.7.0"]
+                               [pjstadig/humane-test-output "0.7.1"]
                                [leiningen "2.5.3"]
-                               [figwheel "0.4.1" :exclusions [org.clojure/clojure]]
+                               [figwheel "0.5.0-2" :exclusions [org.clojure/clojure]]
                                ]
                 :plugins      [[lein-figwheel "0.4.1"]]
 
@@ -143,7 +143,7 @@
                                :update-disabled true
                                :expiration      14
                                }}
-   :test       {:dependencies [[pjstadig/humane-test-output "0.7.0"]
+   :test       {:dependencies [[pjstadig/humane-test-output "0.7.1"]
                                [leiningen "2.5.3"]]
                 :repl-options {:init-ns tropology.repl}
                 :injections   [(require 'pjstadig.humane-test-output)
